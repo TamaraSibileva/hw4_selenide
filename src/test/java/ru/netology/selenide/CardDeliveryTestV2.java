cac79ddeaf4e77eab2e69b59bf70047a3b63de19
Tamara Sibileva <sibilevato@gmail.com>
2025-03-19 17:39:20 +1200

    CardDeliveryTest changed

--- src/test/java/ru/netology/selenide/CardDeliveryTestV2.java
+++ src/test/java/ru/netology/selenide/CardDeliveryTestV2.java
@@ -12,19 +12,22 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-public class CardDeliveryTest {
+public class CardDeliveryTestV2 {
     public String generateDate(int days, String pattern) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
-    void shouldReserveTheDeliveryDate() {
+    void shouldReserveTheDeliveryDateAWeekLater() {
         Selenide.open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Кемерово");
-        String planningDay = generateDate(5, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").doubleClick().press(Keys.DELETE).setValue(planningDay);
-        $("[data-test-id='name'] input").setValue("Павлов Антон");
-        $("[data-test-id='phone'] input").setValue("+75412369852");
+        $("[data-test-id='city'] input").setValue("кр");
+        $$(".menu-item_type_block").filter(Condition.visible).findBy(Condition.text("Краснодар")).click();
+        String planningDay = generateDate(7, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").doubleClick().press(Keys.DELETE);
+        if(!generateDate(3, "MM").equals(generateDate(7, "MM"))) $(".calendar__arrow_direction_right[data-step='1']").click();
+        $$(".popup__container .calendar__day").findBy(Condition.text(generateDate(7, "d"))).click();
+        $("[data-test-id='name'] input").setValue("Антонов Павел");
+        $("[data-test-id='phone'] input").setValue("+79865214852");
         $("[data-test-id='agreement']").click();
         $$("button").filter(Condition.visible).last().click();
         $("[data-test-id='notification']").should(Condition.visible, Duration.ofSeconds(15)).should(Condition.text("Успешно! Встреча успешно забронирована на " + planningDay));
--- src/test/java/ru/netology/selenide/CardDeliveryTestV3.java
+++ src/test/java/ru/netology/selenide/CardDeliveryTestV3.java
@@ -12,19 +12,22 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-public class CardDeliveryTest {
+public class CardDeliveryTestV3 {
     public String generateDate(int days, String pattern) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
-    void shouldReserveTheDeliveryDate() {
+    void shouldReserveTheDeliveryDateForTheNextMonth() {
         Selenide.open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Кемерово");
-        String planningDay = generateDate(5, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").doubleClick().press(Keys.DELETE).setValue(planningDay);
-        $("[data-test-id='name'] input").setValue("Павлов Антон");
-        $("[data-test-id='phone'] input").setValue("+75412369852");
+        $("[data-test-id='city'] input").setValue("ха");
+        $$(".menu-item_type_block").filter(Condition.visible).findBy(Condition.text("Хабаровск")).click();
+        String planningDay = generateDate(20, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").doubleClick().press(Keys.DELETE);
+        if(!generateDate(3, "MM").equals(generateDate(20, "MM"))) $(".calendar__arrow_direction_right[data-step='1']").click();
+        $$(".popup__container .calendar__day").findBy(Condition.text(generateDate(20, "d"))).click();
+        $("[data-test-id='name'] input").setValue("Петров Алексей");
+        $("[data-test-id='phone'] input").setValue("+79852145632");
         $("[data-test-id='agreement']").click();
         $$("button").filter(Condition.visible).last().click();
         $("[data-test-id='notification']").should(Condition.visible, Duration.ofSeconds(15)).should(Condition.text("Успешно! Встреча успешно забронирована на " + planningDay));
